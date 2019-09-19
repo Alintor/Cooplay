c22ad652945775d03f04169149a87c4d33d9d05d
Alexandr Ovchinnikov <a.ovchinnikov@robo.finance>
2019-09-19 15:19:14 +0700

    Create project, basic components

---
+++ Cooplay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cooplay
+//
+//  Created by Alexandr on 19/09/2019.
+//  Copyright © 2019 Ovchinnikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, '11.0'
+
+target 'Cooplay' do
+  use_frameworks!
+
+  pod 'Swinject', '~> 2.6.2'
+  pod 'SwinjectStoryboard', '~> 2.2.0'
+  pod 'R.swift', '~> 5.0.3'
+
+  pod 'LightRoute', :git => 'https://github.com/SpectralDragon/LightRoute.git'
+
+end
